98f0f8d96b4e8eafdc8ee84d4a82e49d808ac218
Amit-Saini <amitsaini.ce@gmail.com>
2026-07-09 11:25:50 +0530

    Time: 2 ms (70.22%) | Memory: 55.4 MB (69.14%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,34 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=(nums.length/2);
+        int element=-1;
+        int count=0;
+
         for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                    if(count>n){
-                        return nums[i];
-                    }
-                }
+            if(count==0){
+                element=nums[i];
+            }
+            if(element==nums[i]){
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return -1;
+
+        return element;
+
+        //Question Says that You may assume that the majority element always exists in the array.
+        //But if need to check if element exist or not we need to do below step but as it says it always exists than it would be element finded above
+
+        // for(int i=0;i<nums.length;i++){
+        //     int count1=0;
+        //     if(nums[i]==element){
+        //         count1++;
+        //     }
+        // }
+        // if(count1>nums.length/2){
+        //     return element;
+        // }
+
     }
 }
