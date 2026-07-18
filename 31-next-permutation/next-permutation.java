9c5b88f6e76872721dcb8ab60c72cf7d8095c1c8
Amit-Saini <amitsaini.ce@gmail.com>
2026-07-18 16:31:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (69.77%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,47 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        //Step one to find longest subarray 
-        int i = nums.length - 2;
-        int j = nums.length - 1;
-        int idx = -1;
-
-        while (i >= 0) {
-            if (nums[i] < nums[j]) {
-                idx = i;
+        int idx=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
                 break;
             }
-            i--;
-            j--;
         }
-        if (idx == -1) {
-            i = 0;
-            j = nums.length - 1;
-            while (i < j) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+
+        if(idx==-1){
+            int i=0;
+            int j=nums.length-1;
+            while(i<j){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
                 i++;
                 j--;
             }
             return;
         }
-        for(i = nums.length - 1; i > idx; i--) {
-            if (nums[i] > nums[idx]) {
-                int temp = nums[idx];
-                nums[idx] = nums[i];
-                nums[i] = temp;
+
+        for(int j=nums.length-1;j>idx;j--){
+            if(nums[j]>nums[idx]){
+                int temp=nums[idx];
+                nums[idx]=nums[j];
+                nums[j]=temp;
                 break;
             }
         }
 
-        i = idx + 1;
-        j = nums.length - 1;
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+        int left=idx+1;
+        int right=nums.length-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            right--;
+            left++;
         }
     }
 }
