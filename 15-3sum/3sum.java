f923f540bca0231a5af3e60ce23d5f9f7db7a49b
Amit-Saini <amitsaini.ce@gmail.com>
2026-08-01 10:31:43 +0530

    Time: 901 ms (7.47%) | Memory: 62.3 MB (5.73%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         Arrays.sort(nums);
-        List<List<Integer>> list=new LinkedList<>();
+        Set<List<Integer>> set = new HashSet<>();
 
-        for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=nums.length-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    List<Integer> l=new LinkedList<>();
-                    l.add(nums[i]);
-                    l.add(nums[j]);
-                    l.add(nums[k]);
-                    list.add(l);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
-                }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    k--;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    List<Integer> list = new LinkedList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    set.add(list);
+                    left++;
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else {
+                    left++;
                 }
             }
+
         }
-        return list;
+        return new LinkedList(set);
     }
 }
