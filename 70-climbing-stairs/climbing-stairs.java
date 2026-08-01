97877295c4104778621f5ab2c9ce367829aeba24
Amit-Saini <amitsaini.ce@gmail.com>
2026-08-01 17:30:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.56%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int count(int n,int dp[]){
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        
+        if(dp[n]!=0){
+            return dp[n];
+        }
+    
+        dp[n]=count(n - 1,dp)+ count(n - 2,dp);
+        return dp[n];
+    }
+
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        return count(n, dp);
+    }
+}
