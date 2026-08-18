7564cfeabbec194985cbfc956386549af034831f
Amit-Saini <amitsaini.ce@gmail.com>
2026-08-18 20:11:32 +0530

    Time: 2 ms (97.98%) | Memory: 72.8 MB (5.47%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,31 +1,21 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int arr[] = new int[3];
-        //arr[0] means no. of 5 coins
-        //arr[1] means no. of 10 coins
-        //arr[2] means no. of 20 coins
-
+        int five = 0;
+        int ten = 0;
 
         for (int i = 0; i < bills.length; i++) {
             if (bills[i] == 5) {
-                arr[0]++;
-            } else if (bills[i] == 10) {
-                arr[1]++;
-                if(arr[0]>0){
-                    arr[0]--;
-                }else{
-                    return false;
-                }
+                five++;
+            } else if (bills[i] == 10 && five >= 1) {
+                ten++;
+                five--;
+            } else if (bills[i] == 20 && ten >= 1 && five >= 1) {
+                ten--;
+                five--;
+            } else if (bills[i] == 20 && five >= 3) {
+                five -= 3;
             } else {
-                if(arr[0]>0 && arr[1]>0){
-                    arr[0]--;
-                    arr[1]--;
-                }else if(arr[0]>=3){
-                    arr[0]-=3;
-                }else{
-                    return false;
-                }
-                arr[2]++;
+                return false;
             }
         }
         return true;
